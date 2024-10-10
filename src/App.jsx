d5e4924b50402dd069f3a974cc0c99037aa67857
rohansaini2102 <138979578+rohansaini2102@users.noreply.github.com>
2024-10-10 17:03:26 +0530

    adding vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Products from './components/Products';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer'
 import ChatUsButton from './components/ChatUsButton';
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <div className="font-sans">
       <Header />
+      <Analytics/>
       <HeroSection />
       <Products />
       <Testimonials />
